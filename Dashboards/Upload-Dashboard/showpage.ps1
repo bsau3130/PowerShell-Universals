40ba2466e9576a4b7a2e9ef66dfd7a5211f9915c
Robin Stolpe <robin@stolpes.nu>
2021-08-14 22:22:26 +0200

    .

--- Dashboards/Upload-Dashboard/showpage.ps1
+++ Dashboards/Upload-Dashboard/showpage.ps1
@@ -5,7 +5,31 @@ $Folder3 = "D:\Upload\Folder3"
 $Folder4 = "D:\Upload\Folder4"
 
 
-function Get-DelBTN {
+function New-UploadBTN {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory)][string]$Path,
+        [Parameter(Mandatory)][string]$RefreshID
+    )
+
+    New-UDUpload -Text 'Upload' -OnUpload {
+        try {
+            $Path = (Get-UDElement -Id "Select").value
+            $Data = $Body | ConvertFrom-Json 
+            $bytes = [System.Convert]::FromBase64String($Data.Data)
+            [System.IO.File]::WriteAllBytes("$Path\$($Data.Name)", $bytes)
+            Show-UDToast -Message "The file $($Data.Name) are now uploaded to $($Path)!" -MessageColor 'green' -Theme 'light' -TransitionIn 'bounceInUp' -CloseOnClick -Position center -Duration 2000
+            Sync-UDElement -Id $RefreshID
+            Hide-UDModal
+        }
+        catch {
+            Show-UDToast -Message "$($PSItem.Exception)" -MessageColor 'red' -Theme 'light' -TransitionIn 'bounceInUp' -CloseOnClick -Position center -Duration 2000
+            Break
+        }
+    }
+}
+
+function New-DelBTN {
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory)][string]$Folder,
@@ -37,6 +61,7 @@ New-UDGrid -Spacing '1' -Container -Content {
     New-UDGrid -Item -MediumSize 4 -Content {
         # README 2
         New-UDCard -Title 'Folder 1' -Content {
+            New-UploadBTN -Path $Folder1 -RefreshID "Folder1"
             New-UDDynamic -Id 'Folder1' -content {
                 $FilesFolder1 = Get-ChildItem -Path $Folder1 | select-object -ExpandProperty name
                 New-UDList -Content {
@@ -45,7 +70,7 @@ New-UDGrid -Spacing '1' -Container -Content {
                             # README 3
                             Invoke-UDRedirect "https://EXAMPLE/Upload/Folder1/$1Doc"
                         }
-                        Get-DelBTN -Folder $Folder1 -File $1Doc -RefreshID "Folder1"
+                        New-DelBTN -Folder $Folder1 -File $1Doc -RefreshID "Folder1"
                     }
                 }
             } -LoadingComponent {
@@ -56,6 +81,7 @@ New-UDGrid -Spacing '1' -Container -Content {
 
     New-UDGrid -Item -MediumSize 4 -Content {
         New-UDCard -Title 'Folder 2' -Content {
+            New-UploadBTN -Path $Folder2 -RefreshID "Folder2"
             New-UDDynamic -Id 'Folder2' -content {
                 $FilesFolder2 = Get-ChildItem -Path $Folder2 | select-object -ExpandProperty name
                 New-UDList -Content {
@@ -63,7 +89,7 @@ New-UDGrid -Spacing '1' -Container -Content {
                         New-UDListItem -Label "$2Doc" -Icon (New-UDIcon -Icon file_download -Size lg) -OnClick {
                             Invoke-UDRedirect "https://EXAMPLE/Upload/Folder2/$2Doc"
                         }
-                        Get-DelBTN -Folder $Folder2 -File $2Doc -RefreshID "Folder2"
+                        New-DelBTN -Folder $Folder2 -File $2Doc -RefreshID "Folder2"
                     }
                 }
             } -LoadingComponent {
@@ -74,6 +100,7 @@ New-UDGrid -Spacing '1' -Container -Content {
     
     New-UDGrid -Item -MediumSize 4 -Content {
         New-UDCard -Title 'Folde 3' -Content {
+            New-UploadBTN -Path $Folder3 -RefreshID "Folder3"
             New-UDDynamic -Id 'Folder3' -content {
                 $FilesFolder3 = Get-ChildItem -Path $Folder3 | select-object -ExpandProperty name
                 New-UDList -Content {
@@ -81,7 +108,7 @@ New-UDGrid -Spacing '1' -Container -Content {
                         New-UDListItem -Label "$3Doc" -Icon (New-UDIcon -Icon file_download -Size lg) -OnClick {
                             Invoke-UDRedirect "https://EXAMPLE/Upload/Folder3/$3Doc"
                         }
-                        Get-DelBTN -Folder $Folder3 -File $3Doc -RefreshID "Folder3"
+                        New-DelBTN -Folder $Folder3 -File $3Doc -RefreshID "Folder3"
                     }
                 }
             } -LoadingComponent {
@@ -92,6 +119,7 @@ New-UDGrid -Spacing '1' -Container -Content {
 
     New-UDGrid -Item -MediumSize 4 -Content {
         New-UDCard -Title 'Folder 4' -Content {
+            New-UploadBTN -Path $Folder4 -RefreshID "Folder4"
             New-UDDynamic -Id 'Folder4' -content {
                 $FilesFolder4 = Get-ChildItem -Path $Folder4 | select-object -ExpandProperty name
                 New-UDList -Content {
@@ -99,7 +127,7 @@ New-UDGrid -Spacing '1' -Container -Content {
                         New-UDListItem -Label "$4Doc" -Icon (New-UDIcon -Icon file_download -Size lg) -OnClick {
                             Invoke-UDRedirect "https://EXAMPLE/Upload/Folder4/$4Doc"
                         }
-                        Get-DelBTN -Folder $Folder4 -File $4Doc -RefreshID "Folder4"
+                        New-DelBTN -Folder $Folder4 -File $4Doc -RefreshID "Folder4"
                     }
                 }
             } -LoadingComponent {
